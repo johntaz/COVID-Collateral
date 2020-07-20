39aee960ba6efcb1581bd4a38b7366d915c00a7f
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2020-07-20 10:02:43 +0100

    Update app.R multiple tabs per stratifier

--- code/shiny_app/covid_collateral_shiny/app.R
+++ code/shiny_app/covid_collateral_shiny/app.R
@@ -11,28 +11,10 @@ here()
 data_files <- list.files(here::here("data/"), pattern = "*RData")
 lapply(here::here("data",data_files), load,.GlobalEnv)
 
-## bodge grid
-namestoinclude <- c("North East","North West","Yorkshire & the Humber","East Midlands","West Midlands","Eastern","South West","South Central","London","South East","Northern Ireland")
-southcentral <- bind_cols("row" = 7, "col" = 3, "name" = "South Central", "code" = "UKK")
-my_grid <- geofacet::uk_regions1 %>%
-	filter(name %in% namestoinclude) %>%
-	bind_rows(southcentral)
-
 # build main database to plot that groups everything ----------------------
-df_for_app <- df_figureRegion %>% 
-	mutate(outcome = "Diabetes") %>%
-	bind_rows(mutate(df_CMI_figureRegion, outcome = "CMI")) %>%
-	bind_rows(mutate(df_CMI_figureRegion, outcome = "SMI")) %>%
-	bind_rows(mutate(df_CMI_figureRegion, outcome = "CVD")) %>%
-	bind_rows(mutate(df_CMI_figureRegion, outcome = "Breathe")) %>%
-	bind_rows(mutate(df_CMI_figureRegion, outcome = "Alcohol")) 
-
-
-regions <- as.list(unique(df_for_app$name))
-outcomes <- as.list(unique(df_for_app$outcome))
+stratifiers <- stringr::str_to_title(c("gender", "age"))
 
 apptitle <- "COVID-Collateral"
-
 
 # user defined ggplot theme -----------------------------------------------
 theme_collateral <- function (base_size = 11, base_family = ""){
@@ -71,19 +53,14 @@ ui <- shinyUI(
 		 ),
 		tabPanel("Plot",
 						 sidebarLayout(
-						 	position = "left",
+						 	position = "right",
 						 	## make the sidebar
 						 	sidebarPanel(
 						 		helpText("Data can be shown for several outcomes simultaneously or one at a time by checking the boxes below"),
 						 		checkboxGroupInput("var", 
 						 											 label = "Choose outcome variables to display", 
-						 											 choices = outcomes,
-						 											 selected = outcomes[[1]]),
-						 		selectInput("regions", 
-						 								multiple = TRUE,
-						 								label = "Regions to display", 
-						 								choices = regions,
-						 								selected = regions[[1]]),
+						 											 choices = as.list(unique(figureAge_shiny$outcome)),
+						 											 selected = as.list(unique(figureAge_shiny$outcome))[[1]]),
 						 		dateRangeInput("dates", 
 						 									 label = "Date range:", 
 						 									 start = "2017-01-01", 
@@ -97,12 +74,41 @@ ui <- shinyUI(
 						 	),
 						 	## make the main panel
 						 	mainPanel(
-						 		fluidRow(
-						 			column(12, plotOutput("mainplot"))
+						 		tabsetPanel(id = "stratifier",
+						 		tabPanel("Age",
+						 						 sidebarLayout(
+						 						 	position = "left",
+						 						 	sidebarPanel(
+						 						 		selectInput("labAge", 
+						 						 								multiple = TRUE,
+						 						 								label = "Age", 
+						 						 								choices = as.list(unique(figureAge_shiny$labels)),
+						 						 								selected = as.list(unique(figureAge_shiny$labels)))
+						 						 	),
+						 						 	mainPanel(
+						 								plotOutput("mainplot1")
+						 						 )
+						 					)
+						 				),
+						 		tabPanel("Gender",
+						 						 sidebarLayout(
+						 						 	position = "left",
+						 						 	sidebarPanel(
+						 						 		selectInput("labGender", 
+						 						 								multiple = TRUE,
+						 						 								label = "Gender", 
+						 						 								choices = as.list(unique(figureGender_shiny$labels)),
+						 						 								selected = as.list(unique(figureGender_shiny$labels)))
+						 						 	),
+						 						 	mainPanel(
+						 								plotOutput("mainplot2")
+						 						 )
+						 					)
+						 				)
 						 		)
-						 	)
+						 		)
 						 )
-		),
+					),
 		tabPanel("About",
 						 fluidRow(
 						 	column(8,
@@ -114,33 +120,56 @@ ui <- shinyUI(
 )
 
 server <- function(input, output, session){
+	v <- reactiveValues(plot = NULL)
+	
+	observeEvent(input$stratifier, {
+		v$plot <- NULL
+	})  
 	## define df to plot as reactive
 	df_shiny <- reactive({
-		dv_shiny <- df_for_app %>% 
+		data <- if (input$stratifier == "Age") {
+			figureAge_shiny
+		} else if (input$stratifier == "Gender"){
+			figureGender_shiny
+		}
+
+		labs_to_find <- if (input$stratifier == "Age") {
+			input$labAge
+		} else if (input$stratifier == "Gender"){
+			input$labGender
+		}
+		
+		df_shiny <- data %>% 
 			filter(weekDate >= format(input$dates[1]) & weekDate <= format(input$dates[2])) %>% 
-			filter(name %in% input$regions) %>%
+			filter(labels %in% labs_to_find) %>%
 			filter(outcome %in% input$var) %>%
-			mutate_at("name", ~as.factor(.))
+			mutate_at("labels", ~as.factor(.))
 	})
-	
+		
 	## set up reactive values to store plot
 	v <- reactiveValues(plot = NULL)
 	
 	## if runPlot button is pressed then build the plot
 	observeEvent(input$runPlot, {
-		v$plot <- ggplot(df_shiny(), aes(x=weekDate, y=model_out, group = name, colour = name)) +
+		v$plot <- ggplot(df_shiny(), aes(x=weekDate, y=model_out, group = labels, colour = labels)) +
 			geom_line() +
-			xlab("Time") +
+			xlab("Date") +
 			ylab("Proportion Overall") +
-			theme_classic() +
 			theme(axis.text.x = element_text(angle=60, hjust=1)) +
-			#scale_x_continuous(breaks = c(0,52, 104, 156, 167, 208), labels = c("2017","2018","2019","2020","Lockdown","2021")) +
 			labs(colour = "Region") +
 			facet_wrap(~outcome, ncol = 3) +
 			theme_collateral()
 	})
 	
-	output$mainplot <- renderPlot({
+	output$mainplot1 <- renderPlot({
+		if (is.null(v$plot)) return()
+		if(input$lockdownLine) {
+			v$plot + geom_vline(xintercept = as.Date("2020-03-28"), linetype = "dashed", col = 2)
+		}else{
+			v$plot
+		}
+	})
+	output$mainplot2 <- renderPlot({
 		if (is.null(v$plot)) return()
 		if(input$lockdownLine) {
 			v$plot + geom_vline(xintercept = as.Date("2020-03-28"), linetype = "dashed", col = 2)
@@ -149,4 +178,5 @@ server <- function(input, output, session){
 		}
 	})
 }
+
 shinyApp(ui, server)
