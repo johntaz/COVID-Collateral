eff7e2f2f1b0e179a7142ce7038286e248529d67
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2020-10-12 14:08:37 +0100

    App - add select all, ITS panel,

--- code/shiny_app/covid_collateral_shiny/app.R
+++ code/shiny_app/covid_collateral_shiny/app.R
@@ -1,6 +1,6 @@
 #pacman::p_load(shiny, shinythemes, dplyr, rmarkdown, ggplot2, here)
 library(shiny)
-library(shinythemes)
+#library(shinythemes)
 library(dplyr)
 library(rmarkdown)
 library(ggplot2)
@@ -12,12 +12,15 @@ here()
 data_file <- list.files(here::here("data/"), pattern = "*.RData")
 lapply(here::here("data",data_file), load,.GlobalEnv)
 
+## change gender to sex 
+refactored_shiny <- refactored_shiny %>%
+	mutate_at("stratifier", ~ifelse(. == "gender", "sex", .))
 ## get unique levels to label choice boxes in app
+sex_choice <- unique(filter(refactored_shiny, stratifier == "sex")$category) %>% as.list()
 ethnicity_choice <- unique(filter(refactored_shiny, stratifier == "ethnicity")$category) %>% as.list()
-gender_choice <- unique(filter(refactored_shiny, stratifier == "gender")$category) %>% as.list()
 region_choice <- unique(filter(refactored_shiny, stratifier == "region")$category) %>% as.list()
 age_choice <- unique(filter(refactored_shiny, stratifier == "age")$category) %>% as.list()
-
+outcome_choices_reordered <- unique(refactored_shiny$outcome)[c(7,1,2,6,8,11,12,13,4,9,10,14,15,16,3,5)]
 apptitle <- "COVID-Collateral"
 
 # user defined ggplot theme -----------------------------------------------
@@ -46,35 +49,38 @@ theme_collateral <- function (base_size = 11, base_family = ""){
 # user interface ----------------------------------------------------------
 ui <- shinyUI(
 	navbarPage(
-		#theme = shinytheme("cerulean"),
 		theme = "bootstrap.min.css",
-		#title = apptitle,
 		 title = div(
-		 	a(img(src="lshtm.png", height = "20px", align = "top"), 
-		 				href="https://lshtm.ac.uk/"),
+		 	a(img(src="collateral_logo.pdf", height = "35px", align = "top"), 
+		 				href="https://www.lshtm.ac.uk/research/centres-projects-groups/electronic-health-records"),
 		 	span(apptitle)
 		 ),
+			
 		tabPanel("Plot",
 						 sidebarLayout(
 						 	position = "right",
 						 	## make the sidebar
 						 	sidebarPanel(
+						 		actionButton(inputId = "runPlot", 
+						 								 style = "color: white; background-color:#4682b4", 
+						 								 label = "Plot"
+						 		),
 						 		helpText("Data can be shown for several outcomes simultaneously or one at a time by checking the boxes below"),
+						 		actionButton("selectAll", label = "Select All"),
+						 		actionButton("deselectAll", label = "Deselect All"),
 						 		checkboxGroupInput("var", 
 						 											 label = "Choose outcome variables to display", 
-						 											 choices = as.list(unique(refactored_shiny$outcome)),
-						 											 selected = as.list(unique(refactored_shiny$outcome))[[1]]
+						 											 choices = as.list(outcome_choices_reordered),
+						 											 selected = as.list(outcome_choices_reordered)
 						 											 ),
+						 		helpText("Data can be shown from January 2017 to July 2020"),
 						 		dateRangeInput("dates", 
 						 									 label = "Date range:", 
-						 									 start = "2017-01-01", 
+						 									 start = "2020-01-01", 
 						 									 end = "2020-07-01"),
 						 		checkboxInput("lockdownLine",
 						 									label = "Display lockdown timing", 
-						 									value = FALSE),
-						 		actionButton(inputId = "runPlot", 
-						 								 label = "Plot"
-						 		)
+						 									value = FALSE)
 						 	),
 						 	## make the main panel
 						 	mainPanel(
@@ -121,15 +127,15 @@ ui <- shinyUI(
 						 						 )
 						 					)
 						 				),
-						 		tabPanel("Gender",
+						 		tabPanel("Sex",
 						 						 sidebarLayout(
 						 						 	position = "left",
 						 						 	sidebarPanel(
-						 						 		selectInput("labGender", 
+						 						 		selectInput("labSex", 
 						 						 								multiple = TRUE,
-						 						 								label = "Gender", 
-						 						 								choices = gender_choice,
-						 						 								selected = gender_choice
+						 						 								label = "Sex", 
+						 						 								choices = sex_choice,
+						 						 								selected = sex_choice
 						 						 		)
 						 						 	),
 						 						 	mainPanel(
@@ -157,6 +163,27 @@ ui <- shinyUI(
 						 		)
 						 )
 					),
+		tabPanel("ITS",
+	 				 sidebarLayout(
+	 				 	position = "right",
+	 				 	## make the sidebar
+	 				 	sidebarPanel(
+	 				 		helpText("Testing 1,2,3..."),
+	 				 		checkboxGroupInput("Lockdown start", 
+	 				 											 label = "Choose outcome variables to display", 
+	 				 											 choices = as.list(unique(refactored_shiny$lockdown)),
+	 				 											 selected = as.list(unique(refactored_shiny$lockdown))
+	 				 		),
+	 				 		actionButton(inputId = "runITS", 
+	 				 								 label = "Testing"
+	 				 		)
+	 				 	),
+	 				 	## make the main panel
+						 	mainPanel(
+						 		uiOutput("pdfview")
+						 	)
+						)
+		),
 		tabPanel("About",
 						 fluidRow(
 						 	column(8,
@@ -168,6 +195,30 @@ ui <- shinyUI(
 )
 
 server <- function(input, output, session){
+	## is "select all" ticked? 
+	#* This observer will update checkboxes 1 - 4 to TRUE whenever selectAll is clicked
+	observeEvent(
+		eventExpr = input$selectAll,
+		handlerExpr = 
+		{
+		 	updateCheckboxInput(session = session, 
+		 											inputId = "var", 
+		 											value = outcome_choices_reordered)
+		}
+	)
+	
+	#* This observer will update checkboxes 1 - 4 to FALSE whenever deselectAll is clicked
+	observeEvent(
+		eventExpr = input$deselectAll,
+		handlerExpr = 
+		{
+			updateCheckboxInput(session = session, 
+													inputId = "var", 
+													value = NA)
+		}
+	)
+	
+	
 	v <- reactiveValues(plot = NULL)
 	
 	observeEvent(input$stratifier, {
@@ -182,8 +233,8 @@ server <- function(input, output, session){
 			input$labAge
 		} else if (input$stratifier == "Region"){
 			input$labRegion
-		} else if (input$stratifier == "Gender"){
-			input$labGender
+		} else if (input$stratifier == "Sex"){
+			input$labSex
 		} else if (input$stratifier == "Ethnicity"){
 			input$labEthnicity
 		} else (1)
@@ -206,14 +257,14 @@ server <- function(input, output, session){
 			ylab("Proportion Overall") +
 			theme(axis.text.x = element_text(angle=60, hjust=1)) +
 			#labs(colour = "Region") +
-			facet_wrap(~outcome, ncol = 3, scales = "free") +
+			facet_wrap(~outcome, ncol = 2, scales = "free") +
 			theme_collateral()
 	})
 	
 	output$mainplot1 <- renderPlot({
 		if (is.null(v$plot)) return()
 		if(input$lockdownLine) {
-			v$plot + geom_vline(xintercept = as.Date("2020-03-16"), linetype = "dashed", col = 2) +
+			v$plot + geom_vline(xintercept = as.Date("2020-03-23"), linetype = "dashed", col = 2) +
 				theme(legend.position = "none")
 		}else{
 			v$plot +
@@ -223,7 +274,7 @@ server <- function(input, output, session){
 	output$mainplot2 <- renderPlot({
 		if (is.null(v$plot)) return()
 		if(input$lockdownLine) {
-			v$plot + geom_vline(xintercept = as.Date("2020-03-16"), linetype = "dashed", col = 2)
+			v$plot + geom_vline(xintercept = as.Date("2020-03-23"), linetype = "dashed", col = 2)
 		}else{
 			v$plot
 		}
@@ -231,7 +282,7 @@ server <- function(input, output, session){
 	output$mainplot3 <- renderPlot({
 		if (is.null(v$plot)) return()
 		if(input$lockdownLine) {
-			v$plot + geom_vline(xintercept = as.Date("2020-03-16"), linetype = "dashed", col = 2)
+			v$plot + geom_vline(xintercept = as.Date("2020-03-23"), linetype = "dashed", col = 2)
 		}else{
 			v$plot 
 		}
@@ -239,7 +290,7 @@ server <- function(input, output, session){
 	output$mainplot4 <- renderPlot({
 		if (is.null(v$plot)) return()
 		if(input$lockdownLine) {
-			v$plot + geom_vline(xintercept = as.Date("2020-03-16"), linetype = "dashed", col = 2)
+			v$plot + geom_vline(xintercept = as.Date("2020-03-23"), linetype = "dashed", col = 2)
 		}else{
 			v$plot
 		}
@@ -247,11 +298,19 @@ server <- function(input, output, session){
 	output$mainplot5 <- renderPlot({
 		if (is.null(v$plot)) return()
 		if(input$lockdownLine) {
-			v$plot + geom_vline(xintercept = as.Date("2020-03-16"), linetype = "dashed", col = 2)
+			v$plot + geom_vline(xintercept = as.Date("2020-03-23"), linetype = "dashed", col = 2)
 		}else{
 			v$plot
 		}
 	})
+	
+	## plot a pdf image
+	observeEvent(input$runITS, {
+		output$pdfview <- renderUI({
+			tags$iframe(style="height:600px; width:100%", src="Figure3_its_backdata_full.pdf")
+		})
+	})
+	
 }
 
-shinyApp(ui, server)
+shinyApp(ui, server, options = list(height = 1300))
